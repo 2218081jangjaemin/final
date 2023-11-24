262f00f0a3dc6fa22bc7a9927418dbd4b893fb81
2218081jangjaemin <144421522+2218081jangjaemin@users.noreply.github.com>
2023-11-24 16:25:50 +0900

    마지막 퀴즈 입니다.
    마지막 퀴즈 입니다.

---
+++ 마지막 퀴즈.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+from sklearn import tree
+from sklearn.model_selection import KFold, cross_val_score
+import matplotlib.pyplot as plt
+from pandas.plotting import scatter_matrix
+data = pd.read_csv('./data/09_irisdata.csv', names=['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class'])
+print(data.shape)
+print(data.describe())
+print(data.groupby('class').size())
+scatter_matrix(data)
+plt.savefig('./result/scatter_matrix.png')
+X = data.iloc[:, 0:4]
+Y = data.iloc[:, 4]
+model = tree.DecisionTreeClassifier()
+kfold = KFold(n_splits=10, random_state=123)
+results = cross_val_score(model, X, Y, cv=kfold, scoring='accuracy')
+print('Accuracy: %.3f%% (%.3f%%)' % (results.mean()*100.0, results.std()*100.0))
